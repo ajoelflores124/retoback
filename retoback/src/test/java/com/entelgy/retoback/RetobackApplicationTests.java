4d20803d0f179b3c96a201b0a6f275dd7ffe19a9
angelflores124 <ajoelflores124@gmail.com>
2021-03-19 21:49:31 -0500

    initializer

---
+++ retoback/src/test/java/com/entelgy/retoback/RetobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entelgy.retoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
